3700f0f1b836c1679b456b9af7bb746cf8717cb2
BLeCorfec <bruno.le_corfec@mines-paristech.fr>
2019-10-31 16:13:32 +0100

    "ajout du choix de la direction du gradient"

--- programme math info.py
+++ programme math info.py
@@ -1,4 +1,8 @@
 
+import autograd
+from autograd import numpy as np
+import matplotlib.pyplot as plt
+
 def find_seed(g, c = 0, eps = 2.0**(-26)):
     maxi = 1.0
     mini = 0.0
@@ -13,9 +17,6 @@ def find_seed(g, c = 0, eps = 2.0**(-26)):
                 mini = milieu
         return (maxi + mini)/2.0
 
-import autograd
-from autograd import numpy as np
-import matplotlib.pyplot as plt
 
 def grad(f, x, y):
     g = autograd.grad
@@ -24,6 +25,7 @@ def grad(f, x, y):
 def jacob(f, x, y):
     j = autograd.jacobian
     return np.c_[j(f, 0)(x, y), j(f, 1)(x, y)]
+
 def newton(F,X0):
     """X0 doit être un array"""
     Xk=X0
@@ -41,8 +43,6 @@ def newton(F,X0):
         Xkp1 = Xkp1 - J2
         
     return Xkp1
-
-
 
 
 def simple_contour (f, c = 0.0, delta = 0.01):
@@ -67,15 +67,49 @@ def simple_contour (f, c = 0.0, delta = 0.01):
         lsy += [Xkp1[1]]
     return lsx, lsy
 
+def sens_grad(gradx, grady):
+    if gradx>0:
+            if grady>0:
+                return -1
+            return 1
+    elif grady>0:
+        return -1
+    return 1
+
+
+def simple_contour2 (f, c = 0.0, delta = 0.01):
+    t = find_seed(f, c, eps = 2.0**(-26))
+    if t == None:
+        return [],[]
+    lsx = [0.0]
+    lsy = [t]
+    x0 = lsx[-1]
+    y0 = lsy[-1]
+    gradient = grad(f, x0, y0)
+    a=sens_grad(gradient[0],gradient[1])
+    def F(x,y):
+            return np.r_[f(x, y) - c, (x-x0)**2 + (y-y0)**2 - delta**2]
+    a=sens_grad(gradient[0],gradient[1])
+    while lsx[-1] >= 0.0 and lsx[-1] <= 1.0 and lsy[-1] >= 0.0 and lsy[-1] <= 1.0:
+        x0 = lsx[-1]
+        y0 = lsy[-1]
+        X0 = np.r_[x0, y0]
+        gradient = grad(f, x0, y0)
+        coeffdir = np.r_[-a*gradient[1], a*gradient[0]]
+        Xk = X0 + delta / np.linalg.norm(coeffdir) * coeffdir
+        Xkp1 = newton(F,Xk)
+        lsx += [Xkp1[0]]
+        lsy += [Xkp1[1]]
+    return lsx, lsy
 
 
 def f(x,y):
-    return ((x-0.3)**2 + (y-0.7)**2)/2
+    return -((x-0.3)**2 + (y-0.7)**2)/2
 def g(x,y):
-    return ((x-0.3)**2 + (y-0.7)**2)/2
+    return -((x-0.3)**2 + (y-0.7)**2)/2
 
 
-a=simple_contour(f,0.1)
+a=simple_contour(f,-0.1)
 plt.plot(a[0],a[1], marker='x')
 gnp=np.vectorize(g)
 X = np.arange(0, 1, 0.01)
@@ -83,6 +117,6 @@ Y = np.arange(0, 1, 0.01)
 X, Y = np.meshgrid(X, Y)
 Z = gnp(X, Y)
 plt.axis('equal')
-plt.contour(X, Y, Z, [0.1])
+plt.contour(X, Y, Z, [-0.1])
 plt.show()
 
